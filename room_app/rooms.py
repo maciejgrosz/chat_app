599d8e85e687e7e8ee906c2ac79a7c6262da227b
Maciej Groszyk <maciej.groszyk8@gmail.com>
2022-08-27 23:52:05 +0200

    add init script, and dockerize mysql db

--- room_app/rooms.py
+++ room_app/rooms.py
@@ -1,18 +1,19 @@
 from flask import Flask, redirect, url_for, render_template, request
+import datetime
 
 from flaskext.mysql import MySQL
 
 app = Flask(__name__)
 
 mysql = MySQL()
-app.config["MYSQL_DATABASE_HOST"] = "172.17.0.2"
+app.config["MYSQL_DATABASE_HOST"] = "172.17.0.3"
 app.config["MYSQL_DATABASE_USER"] = "app"
-app.config["MYSQL_DATABASE_PASSWORD"] = "sql_pass"
-app.config["MYSQL_DATABASE_DB"] = "MyDB"
+app.config["MYSQL_DATABASE_PASSWORD"] = "pass"
+app.config["MYSQL_DATABASE_DB"] = "chat_db"
 mysql.init_app(app)
 
 
-@app.route("/<id>", methods=["GET", "POST"])
+@app.route("/<id>", methods=["GET"])
 def home(id):
     return render_template("index.html")
 
@@ -21,21 +22,24 @@ def home(id):
 def api_chat(id):
     conn = mysql.connect()
     cur = conn.cursor()
+
     if request.method == "GET":
-        cur.execute(f"SELECT * FROM chat_table WHERE room_id={id}")
+        cur.execute(f"SELECT * FROM chat WHERE ID={id}")
         data = cur.fetchall()
-        chat_history = [f"({record[1]}): {record[2]}" for record in data]
+        chat_history = [f"{record[3]}~({record[1]}): {record[2]}" for record in data]
         if not chat_history:
             chat_history.append("EMPTY HISTORY")
         return "\n".join(chat_history)
+
     if request.method == "POST":
         username = request.form.get("username")
         message = request.form.get("msg")
+        date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") 
         cur.execute(
-            f'INSERT INTO chat_table (room_id, username, message) VALUES ({id}, "{username}", "{message}")'
+            f"INSERT INTO chat (ID,username,message,date) VALUES ('{id}','{username}','{message}','{date}');"
         )
         conn.commit()
-        return f"{message}"
+        return f'{message}'
 
 
 if __name__ == "__main__":
